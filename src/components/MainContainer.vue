d35292a3c06b958067ce90f52539459219a16550
AgoDe <ago.defelice@gmail.com>
2022-02-08 15:18:46 +0100

    print banner

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,5 +1,25 @@
 <template>
-    <main></main>
+    <main>
+        
+
+        <div class="jumbotron">
+            <div class="container">
+
+
+            </div>
+        </div>
+
+        <div class="banner">
+            <div class="container">
+                <p>How to Enroll Your Child to a Class?</p>
+                <div class="button">learn more</div>
+            </div>
+        </div>
+
+
+
+      
+    </main>
 </template>
 
 <script>
@@ -9,5 +29,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/main.scss';
+    main {
+        
+
+        .jumbotron {
+            
+           
+            
+        } // end of jumbotron
+        .banner {
+            background: $purple;
+            color: $sugar;
+            
+            .container {
+                height: 70px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                p {
+                    font-size: 19px;
+                }
+                .button {
+                    border: white 1px solid;
+                }
+            }
+        } // end of banner
+    } 
+       
 
 </style>
