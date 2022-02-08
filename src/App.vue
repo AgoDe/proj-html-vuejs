b1a82ebfcb861cb689404f93604d2791be83d1e1
AgoDe <ago.defelice@gmail.com>
2022-02-08 12:06:16 +0100

    create main & variables style files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,97 @@
 <template>
   <div id="app">
-    
+
+      <!-- header container -->
+      <header-container
+      />
+      <!-- end of header container -->
+
+      <!-- main container  -->
+      <main-container
+      />
+      <!-- end of main container  -->
+
+      <!-- footer container  -->
+      <footer-container
+      />
+      <!-- end of footer container  -->
+
+
   </div>
 </template>
 
 <script>
+import FooterContainer from './components/FooterContainer.vue'
+import HeaderContainer from './components/HeaderContainer.vue'
+import MainContainer from './components/MainContainer.vue'
 
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderContainer,
+    MainContainer,
+    FooterContainer
+  },
+  data() {
+    return {
+      // Array for navbar items
+      navItem: [
+        {
+          item: 'Home',
+          path: 'home-2.png',
+          active: true,
+          pathActive:'home.png',
+        },
+        {
+          item: 'Pages',
+          path: 'document-2.png',
+          active: false,
+          pathActive:'document.png',
+
+        },
+        {
+          item: 'blog',
+          path: 'printer-2',
+          active: true,
+          pathActive:'printer.png',
+
+        },
+        {
+          item: 'Shop',
+          path: 'cart-2.png',
+          active: true,
+          pathActive:'cart.png',
+
+        },
+        {
+          item: 'Shortcodes',
+          path: 'lab-2.png',
+          active: true,
+          pathActive:'lab.png',
+
+        },
+        {
+          item: 'Support',
+          path: 'chat-2.png',
+          active: true,
+          pathActive:'chat.png',
+
+        },
+        {
+          item: 'Contact',
+          path: 'envelope-2.png',
+          active: true,
+          pathActive:'envelope.png',
+
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '@/styles/main.scss';
 
 </style>
---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <div class="container"> 
+
+            <img src="../assets/images/logo.png" alt="logo">
+
+            <nav></nav>
+
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/main.scss';
+
+
+</style>
